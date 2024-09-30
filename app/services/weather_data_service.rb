42ac07d5132fa3cef5c163538222dce59af0d394
Ivan <chivox.ivan@gmail.com>
2024-09-29 23:28:38 -0500

    updated gitignore and now using imperial metric system for temperature

--- app/services/weather_data_service.rb
+++ app/services/weather_data_service.rb
@@ -4,7 +4,7 @@ class WeatherDataService < ApplicationService
   def initialize(latitude, longitude)
     @latitude = latitude
     @longitude = longitude
-    @url = "https://api.openweathermap.org/data/2.5/weather?lat=#{@latitude}&lon=#{@longitude}"
+    @url = "https://api.openweathermap.org/data/2.5/weather?lat=#{@latitude}&lon=#{@longitude}&units=imperial"
   end
 
   def call
